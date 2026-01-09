38924b0bc0e5da6c60c63289ff48d3bf5100bf13
Maik Preuss <maik.preuss@gmx.de>
2026-01-09 11:58:01 +0100

    Updates multi-file-upload dependency to stable version
    Updates the "brezo-it/multi-file-upload" dependency from "@dev" to the stable version "^1.0".
    
    This change ensures that the project uses a stable release of the multi-file-upload extension, providing more predictable behavior and reducing the risk of encountering issues associated with development versions.
    
    Additionally, all files belonging to the old path based dependency are removed.

--- packages/multi_file_upload/Configuration/Icons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'multi_file_upload' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:multi_file_upload/Resources/Public/Icons/Extension.svg',
-    ],
-];
--- packages/multi_file_upload/ext_tables.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-defined('TYPO3') or die();
-
-// Backend-related registration can go here (kept minimal on purpose)
